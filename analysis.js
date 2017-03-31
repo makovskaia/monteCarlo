9610dee79535f792363a6c3bb8d363db6d0f3a80
mawa <makovskayamd@gmail.com>
2017-03-31 20:09:19 +0300

    add function "move" that triggers one game round and supporting functions as well

--- analysis.js
+++ analysis.js
@@ -1,1 +1,44 @@
-export default [1,2,3,4];
+
+//players: first = x, val = 5;
+		 //second = o, val 3
+//board: len = 9, board[i] = 0 if empty, 
+                           //= 5 if first,
+                           //= 3 IF second;
+
+let vectors = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+let board = [0,0,0,0,0,0,0,0,0];
+let isFirstMove = true;
+// let log = [{player: 5, firstMove: 0, moves: []}, {player: 5, firstMove: 0, moves: []}];
+
+
+//vector's value = sum of 0||3||5||'s 
+let findVectorValue = (vector) => {
+	let sum = 0;
+	for(let i = 0; i < vector.length; i++){
+		let e = board[vector[i]];
+		sum += e;
+	};
+	return sum;
+};
+
+//returns 0 if not, 1 if first won, 2 if second won, 3 if draw
+let checkIfFinish = () => {
+	let numberOfAviableVectors = 0;
+	for (let i = 0; i < vectors.length; i++){
+		let v = vectors[i];
+		let value = findVectorValue(v);
+		let isAviable = !((value === 6 && isFirstMove) || (value === 10 && !isFirstMove) || value === 8 || value === 13 || value === 11);
+
+		numberOfAviableVectors += isAviable ? 1 : 0;
+		if(value === 15){
+			return 1;
+		}
+		if(value === 9){
+			return 2;
+		}
+	}
+	if( !numberOfAviableVectors){
+		return 3;
+	}
+	return 0;
+};
